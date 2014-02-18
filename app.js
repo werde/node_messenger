77633f9e80213140cd76d9bd5c751910654ea368
werde <dolfaq@gmail.com>
2014-02-18 16:04:27 +0400

    wtf

--- app.js
+++ app.js
@@ -1,71 +1,33 @@
 var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-//var config = require('config');
-//var log = require('lib/log')(module);
-//var mongoose = require('lib/mongoose');
-//var HttpError = require('error').HttpError;
 
 var app = express();
 
-app.engine('ejs', require('ejs-locals'));
-app.set('views', __dirname + '/template');
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(express.favicon());
-
-if (app.get('env') == 'development') {
-	app.use(express.logger('dev'));
-} else {
-	app.use(express.logger('default'));
-}
-
-app.use(express.bodyParser());
-
-app.use(express.cookieParser());
-
-/*var sessionStore = require('lib/sessionStore');
-
-app.use(express.session({
-	secret: config.get('session:secret'),
-	key: config.get('session:key'),
-	cookie: config.get('session:cookie'),
-	store: sessionStore
-}));
-
-app.use(require('middleware/sendHttpError'));
-app.use(require('middleware/loadUser'));
-*/
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
 app.use(app.router);
-
-require('routes')(app);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
 
-app.use(function(err, req, res, next) {
-	if (typeof err == 'number') {
-		err = new HttpError(err);
-	}
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-	if (err instanceof HttpError) {
-		res.sendHttpError(err);
-	} else {
-	if (app.get('env') == 'development') {
-		express.errorHandler()(err, req, res, next);
-	} else {
-		log.error(err);
-		err = new HttpError(500);
-		res.sendHttpError(err);
-		}
-	}
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-var server = http.createServer(app);
-server.listen(80, function(){
-	console.log('Express server listening on port ' + 80);
-});
-
-//var io = require('./socket')(server);
-//app.set('io', io);
